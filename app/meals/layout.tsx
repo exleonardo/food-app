be6f86025994cf0f4285d0af7f20da8f183543c6
Alexander Khomenok <exleonardo@mail.ru>
2024-04-16 23:25:15 +0300

    feat: add layout.tsx

---
+++ app/meals/layout.tsx
@@ -0,0 +1,13 @@
+import { PropsWithChildren } from 'react'
+
+import { NextPage } from 'next'
+
+const MealsLayout: NextPage<PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      <p>Meals Layout</p>
+    </>
+  )
+}
+
+export default MealsLayout
