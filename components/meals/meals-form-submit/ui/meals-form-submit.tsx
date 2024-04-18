8a4016403f7c68d1f455a386d61194cb50e9b476
Alexander Khomenok <exleonardo@mail.ru>
2024-04-18 13:21:15 +0300

    feat: button submit

---
+++ components/meals/meals-form-submit/ui/meals-form-submit.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { useFormStatus } from 'react-dom'
+
+export const MealsFormSubmit = () => {
+  const { pending } = useFormStatus()
+
+  return <button disabled={pending}>{pending ? 'Submitting' : 'Share meal'}</button>
+}
