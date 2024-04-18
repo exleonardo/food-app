b2acbae111996649d5a7303288517c70bd8d8919
Alexander Khomenok <exleonardo@mail.ru>
2024-04-18 13:22:42 +0300

    feat: refactor import

--- lib/meals.ts
+++ lib/meals.ts
@@ -1,4 +1,12 @@
-import sql from 'better-sqlite3'
+import fs from 'fs'
+
+import { MealType } from '@/lib/actions'
+
+const sql = require('better-sqlite3')
+
+const slugify = require('slugify')
+
+import xss from 'xss'
 
 const db = sql('meals.db')
 
@@ -28,4 +36,35 @@ export const getMeals = async (): Promise<MealItemProps[]> => {
 }
 export const getMeal = (slug: string): MealItemProps => {
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug) as MealItemProps
+}
+export const saveMeal = async (meal: MealType): Promise<void> => {
+  meal.slug = slugify(meal.title, { lower: true })
+
+  meal.instructions = xss(meal.instructions)
+
+  const extension = meal.image.name.split('.').pop()
+  const fileName = `${meal.slug}.${extension}`
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`)
+  const bufferedImage = await meal.image.arrayBuffer()
+
+  stream.write(Buffer.from(bufferedImage), error => {
+    if (error) {
+      throw new Error('Saving image failed')
+    }
+  })
+
+  // @ts-ignore
+  meal.image = `/images/${fileName}`
+  db.prepare(
+    `
+  INSERT INTO meals (title, summary, instructions, creator, creator_email, image, slug) VALUES ( 
+         @title,
+         @summary,
+         @instructions,
+         @creator,
+         @creator_email,  
+         @image,
+         @slug) `
+  ).run(meal)
 }
